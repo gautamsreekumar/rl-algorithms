27d539455d960d372e4df9db31d2480ab380beaa
Gautam Sreekumar <gautamsree@gmail.com>
2019-01-14 14:23:54 +0530

    Add files via upload

--- rooms/__init__.py
+++ rooms/__init__.py
@@ -1,1 +1,12 @@
+import logging
+from gym.envs.registration import register
 
+logger = logging.getLogger(__name__)
+
+register(
+    id='FourRooms-v0',
+    entry_point='rooms.envs:FourRooms',
+    timestep_limit=10000,
+    reward_threshold=1.0,
+    nondeterministic = True,
+)
